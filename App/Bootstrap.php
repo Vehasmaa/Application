41a5cb9852d8bd3ce232ecdfee31da3ed6da7bb3
Jurva-Markus Vehasmaa <jurva.vehasmaa@gmail.com>
2023-10-17 18:26:30 +0000

    Added prerouter to bootstrap for future use

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -5,11 +5,15 @@
 // Load generally needed class files
 require_once __DIR__.'/Library/Template.php';
 require_once __DIR__.'/Library/Database.php';
+// Added prerouter here as servivice way - even if it does nothing currently
+require_once __DIR__.'/Core/Prerouter.php';
 require_once __DIR__.'/Core/Router.php';
 
 // Create instance of those classes and save it so globally available
 $GLOBALS['Template'] = new Template;
 $GLOBALS['Database'] = new Database;
+// Addib preroute object to be called later when it does something useful
+$GLOBALS['Prerouter'] = new Prerouter;
 $GLOBALS['Router'] = new Router;
 
 ?>
