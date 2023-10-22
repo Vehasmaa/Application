3b72b81945e5c0ec146832ae3257646926a75d1d
Jurva-Markus Vehasmaa <jurva.vehasmaa@gmail.com>
2023-10-22 14:00:58 +0000

    Phpunit lisätty ja luokkien lataus autoloader

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -2,18 +2,38 @@
 # Micro MVC striimi projekti 18.3.2023 - 
 # Author: Jurva-Markus Vehasmaa
 
-// Load generally needed class files
-require_once __DIR__.'/Library/Template.php';
-require_once __DIR__.'/Library/Database.php';
-// Added prerouter here as servivice way - even if it does nothing currently
-require_once __DIR__.'/Core/Prerouter.php';
-require_once __DIR__.'/Core/Router.php';
 
-// Create instance of those classes and save it so globally available
-$GLOBALS['Template'] = new Template;
-$GLOBALS['Database'] = new Database;
+/**
+ * These requires are obsolete in way they are loaded by autoloeaders now.
+ * 
+ */
+// // Load generally needed class files
+// require_once __DIR__.'/Library/Template.php';
+// require_once __DIR__.'/Library/Database.php';
+// // Added prerouter here as servivice way - even if it does nothing currently
+// require_once __DIR__.'/Core/Prerouter.php';
+// require_once __DIR__.'/Core/Router.php';
+
+// Lets try instanciate Template and database objects
+if(class_exists('Template') && class_exists('Database'))
+{
+    $GLOBALS['Template'] = new Template;
+}
+else
+{
+    echo 'Primary classes missing';
+    exit();
+}
+
 // Addib preroute object to be called later when it does something useful
-$GLOBALS['Prerouter'] = new Prerouter;
-$GLOBALS['Router'] = new Router;
+
+if (class_exists('Prerouter') && class_exists('Router'))
+{
+    $GLOBALS['Prerouter'] = new Prerouter;
+    $GLOBALS['Router'] = new Router;
+} else {
+    echo 'Routing classes not loaded';
+    exit();
+}
 
 ?>
--- index.php
+++ index.php
@@ -2,8 +2,30 @@
 # Micro MVC striimi projekti 18.3.2023 - 
 # Author: Jurva-Markus Vehasmaa
 
+
+
+// Class Autoloaders
+
+spl_autoload_register(function ($className) {
+    // Autoloading logic for App/Library
+    // Do not try include if docent exists
+    if(file_exists(__DIR__ . '/App/Library/' . $className . '.php')){
+            include __DIR__ .'/App/Library/' . $className . '.php';
+    } 
+});
+
+spl_autoload_register(function ($className) {
+    // Autoloading logic for App/Core
+    // Do not try include if docent exists
+    if(file_exists(__DIR__ . '/App/Core/' . $className . '.php')) {
+            include __DIR__ . '/App/Core/' . $className . '.php';
+    }
+});
+
+
 // PHP session system start
 session_start();
+
 
 // Call application setup system
 require_once './App/Bootstrap.php';
